f43dfe958956d67c00c8a68ffeffe29921c73d9f
Koryun <koryun.1996.hh@gmail.com>
2024-10-22 16:58:03 +0400

    fixed crash

--- ProgressHUD/Sources/NoAnimation/SuperAnimation/SuperAnimationView.swift
+++ ProgressHUD/Sources/NoAnimation/SuperAnimation/SuperAnimationView.swift
@@ -3,16 +3,16 @@ import UIKit
 import Lottie
 
 class SuperAnimationView: UIView, InstanceFromNibProtocol {
-	typealias InstanceFromNibType = SuperAnimationView
-	
-	
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var subtitlelabel: UILabel!
-    @IBOutlet weak var cancelLabel: UILabel!
-    @IBOutlet weak var animationView: UIView! {
-		didSet {
+    typealias InstanceFromNibType = SuperAnimationView
+    
+    @IBOutlet weak var titlelabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var cancellabel: UILabel!
+    
+    @IBOutlet weak var lottieAnimationView: UIView! {
+        didSet {
             let anView = LottieAnimationView()
-            animationView.addSubview(anView)
+            lottieAnimationView.addSubview(anView)
             anView.snp.makeConstraints { make in
                 make.edges.equalToSuperview()
             }
@@ -22,12 +22,12 @@ class SuperAnimationView: UIView, InstanceFromNibProtocol {
             anView.backgroundBehavior = .pauseAndRestore
             anView.backgroundColor = .clear
             anView.play()
-		}
-	}
-	
-    @IBOutlet weak var infoView: UIView! {
+        }
+    }
+    
+    @IBOutlet weak var infoview: UIView! {
         didSet {
-            infoView.applyGradient(
+            infoview.applyGradient(
                 isVertical: false,
                 colorArray: [
                     UIColor().hexStringToUIColor(hex: "#FFD39E"),
@@ -38,100 +38,100 @@ class SuperAnimationView: UIView, InstanceFromNibProtocol {
             )
         }
     }
-	
-	@IBOutlet weak var buyButton: GradientButton! {
-		didSet {
-			buyButton.cornerRadius = 20
-			buyButton.startColor = UIColor().hexStringToUIColor(hex: "#687EF8")
-			buyButton.endColor = UIColor().hexStringToUIColor(hex: "#6227CA")
-			buyButton.startPoint = CGPoint(x: 0, y: 0)
-			buyButton.endPoint = CGPoint(x: 1, y: 1)
-            buyButton.setTitle(localizeText(forKey: .subsBuy), for: .normal)
-		}
-	}
-	
-	@IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet weak var buybutton: GradientButton! {
+        didSet {
+            buybutton.cornerRadius = 20
+            buybutton.startColor = UIColor().hexStringToUIColor(hex: "#687EF8")
+            buybutton.endColor = UIColor().hexStringToUIColor(hex: "#6227CA")
+            buybutton.startPoint = CGPoint(x: 0, y: 0)
+            buybutton.endPoint = CGPoint(x: 1, y: 1)
+            buybutton.setTitle(localizeText(forKey: .subsBuy), for: .normal)
+        }
+    }
+    
+    @IBOutlet weak var pricelabel: UILabel!
     
     private let isSmallDevice = UIScreen.main.nativeBounds.height <= 1334
     
-	var continueButtonTapped: (() -> Void)?
-	var closeButtonTapped: (() -> Void)?
-
+    var continueButtonTapped: (() -> Void)?
+    var closeButtonTapped: (() -> Void)?
+    
     var currentTariff: String? {
-		didSet {
-//            let text = currentTariff?.localizedPrice ?? "$\(currentTariff?.price ?? 7.99)"
-
-			priceLabel.text = String(format: localizeText(forKey: .subsPrice), currentTariff ?? "7.99")
-		}
-	}
+        didSet {
+            //            let text = currentTariff?.localizedPrice ?? "$\(currentTariff?.price ?? 7.99)"
+            
+            pricelabel.text = String(format: localizeText(forKey: .subsPrice), currentTariff ?? "7.99")
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        titleLabel.text = localizeText(forKey: .subsTitle)
-        subtitlelabel.text = localizeText(forKey: .subsSub)
-        cancelLabel.text = localizeText(forKey: .subsCancel)
+        titlelabel.text = localizeText(forKey: .subsTitle)
+        subtitleLabel.text = localizeText(forKey: .subsSub)
+        cancellabel.text = localizeText(forKey: .subsCancel)
     }
-	
-	@IBAction func buyButtonTapped(_ sender: UIButton) {
-		buyButton.isEnabled = false
-		continueButtonTapped?()
-	}
-	
-	@IBAction func closeButtonTapped(_ sender: UIButton) {
-		closeButtonTapped?()
-	}
+    
+    @IBAction func buyButtonTapped(_ sender: UIButton) {
+        buybutton.isEnabled = false
+        continueButtonTapped?()
+    }
+    
+    @IBAction func closeButtonTapped(_ sender: UIButton) {
+        closeButtonTapped?()
+    }
 }
 
 extension UIView {
-	func applyGradient(isVertical: Bool, colorArray: [UIColor]) {
-		layer.sublayers?.filter({ $0 is CAGradientLayer }).forEach({ $0.removeFromSuperlayer() })
-		 
-		let gradientLayer = CAGradientLayer()
+    func applyGradient(isVertical: Bool, colorArray: [UIColor]) {
+        layer.sublayers?.filter({ $0 is CAGradientLayer }).forEach({ $0.removeFromSuperlayer() })
         
-		gradientLayer.colors = colorArray.map({ $0.cgColor })
+        let gradientLayer = CAGradientLayer()
         
-		if isVertical {
-			gradientLayer.locations = [0.0, 1.0]
-		} else {
-			gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
-			gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-		}
-		
-		backgroundColor = .clear
-		gradientLayer.frame = bounds
-		gradientLayer.cornerRadius = 12
-		layer.insertSublayer(gradientLayer, at: 0)
-	}
+        gradientLayer.colors = colorArray.map({ $0.cgColor })
+        
+        if isVertical {
+            gradientLayer.locations = [0.0, 1.0]
+        } else {
+            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+            gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        }
+        
+        backgroundColor = .clear
+        gradientLayer.frame = bounds
+        gradientLayer.cornerRadius = 12
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
 
 final class GradientButton: UIButton {
     public override class var layerClass: AnyClass         { CAGradientLayer.self }
     private var gradientLayer: CAGradientLayer             { layer as! CAGradientLayer }
-
+    
     public var startColor: UIColor = .white { didSet { updateColors() } }
     public var endColor: UIColor = .red     { didSet { updateColors() } }
-
+    
     public var startPoint: CGPoint {
         get { gradientLayer.startPoint }
         set { gradientLayer.startPoint = newValue }
     }
-
+    
     public var endPoint: CGPoint {
         get { gradientLayer.endPoint }
         set { gradientLayer.endPoint = newValue }
     }
-
+    
     public var cornerRadius: CGFloat {
         get { layer.cornerRadius }
         set { layer.cornerRadius = newValue }
     }
-
+    
     public var borderWidth: CGFloat {
         get { layer.borderWidth }
         set { layer.borderWidth = newValue }
     }
-
+    
     public var borderColor: UIColor? {
         get { layer.borderColor.flatMap { UIColor(cgColor: $0) } }
         set { layer.borderColor = newValue?.cgColor }
@@ -142,7 +142,7 @@ final class GradientButton: UIButton {
         
         updateColors()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         
