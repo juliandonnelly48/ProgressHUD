026dbbc7df147fbcc84630d8499783ad73ab2b24
Koryun <koryun.1996.hh@gmail.com>
2024-10-23 11:59:36 +0400

    fixed text

--- ProgressHUD/Sources/NoAnimation/ResultAnimation/ResultAnimationView.swift
+++ ProgressHUD/Sources/NoAnimation/ResultAnimation/ResultAnimationView.swift
@@ -98,13 +98,13 @@ class ResultAnimationView: UIView, InstanceFromNibProtocol {
         
         if isTarifPaidAndActive {
             if Storage.isAllFeaturesEnabled, Storage.featuresStates.count == 6 {
-                let attributedStrOne = NSMutableAttributedString(string: String(model?.scn?.title_compl?.dropLast(2) ?? ""), attributes: [
+                let attributedStrOne = NSMutableAttributedString(string: String(model?.scn?.subtitle_anim_compl?.dropLast(2) ?? ""), attributes: [
                     NSAttributedString.Key.foregroundColor: UIColor().hexStringToUIColor(hex: "#000000"),
                     NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .medium)
                 ])
                 let attributedStrTwo = NSMutableAttributedString(string: localizeText(forKey: .subsOn).uppercased(), attributes: [
                     NSAttributedString.Key.foregroundColor: UIColor().hexStringToUIColor(hex: "#65D65C"),
-                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .medium)
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .bold)
                 ])
                 attributedStrOne.append(attributedStrTwo)
                 
@@ -123,13 +123,13 @@ class ResultAnimationView: UIView, InstanceFromNibProtocol {
                     self?.animationView.play()
                 }, animationCache: DefaultAnimationCache.sharedCache)
             } else {
-                let attributedStrOne = NSMutableAttributedString(string: String(model?.scn?.title_compl?.dropLast(2) ?? ""), attributes: [
+                let attributedStrOne = NSMutableAttributedString(string: String(model?.scn?.subtitle_anim_compl?.dropLast(2) ?? ""), attributes: [
                     NSAttributedString.Key.foregroundColor: UIColor().hexStringToUIColor(hex: "#000000"),
                     NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .medium)
                 ])
                 let attributedStrTwo = NSMutableAttributedString(string: localizeText(forKey: .subsOff).uppercased(), attributes: [
                     NSAttributedString.Key.foregroundColor: UIColor().hexStringToUIColor(hex: "#E74444"),
-                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .medium)
+                    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .bold)
                 ])
                 attributedStrOne.append(attributedStrTwo)
                 
@@ -152,13 +152,13 @@ class ResultAnimationView: UIView, InstanceFromNibProtocol {
                 }
             }
         } else {
-            let attributedStrOne = NSMutableAttributedString(string: String(model?.scn?.title_compl?.dropLast(2) ?? ""), attributes: [
+            let attributedStrOne = NSMutableAttributedString(string: String(model?.scn?.subtitle_anim_compl?.dropLast(2) ?? ""), attributes: [
                 NSAttributedString.Key.foregroundColor: UIColor().hexStringToUIColor(hex: "#000000"),
                 NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .medium)
             ])
             let attributedStrTwo = NSMutableAttributedString(string: localizeText(forKey: .subsOff).uppercased(), attributes: [
                 NSAttributedString.Key.foregroundColor: UIColor().hexStringToUIColor(hex: "#E74444"),
-                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .medium)
+                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .bold)
             ])
             attributedStrOne.append(attributedStrTwo)
             
